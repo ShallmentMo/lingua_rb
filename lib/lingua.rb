9c4ecfbf32550137e63186cae3c278b51b5a4f10
ShallmentMo <shallmentmo@gmail.com>
2024-05-21 17:58:45 +0800

    init

---
+++ lib/lingua.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "lingua/version"
+require_relative "lingua/lingua"
+
+module Lingua
+  class Error < StandardError; end
+  # Your code goes here...
+end
