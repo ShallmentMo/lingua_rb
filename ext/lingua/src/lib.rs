3b6fa2ef270958eaa0e44f83f7a6e2972669cb3d
Roger <shallmentmo@gmail.com>
2024-05-24 00:23:42 +0800

    support builder options

--- ext/lingua/src/lib.rs
+++ ext/lingua/src/lib.rs
@@ -1,16 +1,53 @@
+use std::str::FromStr;
+
 use lingua::{Language, LanguageDetector, LanguageDetectorBuilder};
 use magnus::{function, prelude::*, Error, Ruby};
 
-fn detect(subject: String) -> Option<String> {
-    let detector: LanguageDetector = LanguageDetectorBuilder::from_all_languages().build();
-    let detected_language: Option<Language> = detector.detect_language_of(subject);
+fn detect(arguments: magnus::RArray) -> Option<String> {
+    match arguments.len() {
+        1 => {
+            let subject = arguments.shift::<String>().unwrap();
+            let detector: LanguageDetector = LanguageDetectorBuilder::from_all_languages().build();
+            let detected_language: Option<Language> = detector.detect_language_of(subject);
 
-    detected_language.map(|language| language.to_string())
+            detected_language.map(|language| language.to_string())
+        }
+        2 => {
+            let subject = arguments.shift::<String>().unwrap();
+            let options = arguments.shift::<magnus::RHash>().unwrap();
+            let mut builder = match options.fetch::<&str, Vec<String>>("languages") {
+                Ok(languages) => {
+                    let languages: Vec<Language> = languages
+                        .into_iter()
+                        .map(|l| Language::from_str(&l).unwrap())
+                        .collect();
+                    LanguageDetectorBuilder::from_languages(&languages)
+                }
+                Err(_) => LanguageDetectorBuilder::from_all_languages(),
+            };
+            if let Ok(minimum_relative_distance) =
+                options.fetch::<&str, f64>("minimum_relative_distance")
+            {
+                builder.with_minimum_relative_distance(minimum_relative_distance);
+            };
+            if let Ok(true) = options.fetch::<&str, bool>("is_every_language_model_preloaded") {
+                builder.with_preloaded_language_models();
+            };
+            if let Ok(true) = options.fetch::<&str, bool>("is_low_accuracy_mode_enabled") {
+                builder.with_low_accuracy_mode();
+            };
+            let detector = builder.build();
+            let detected_language: Option<Language> = detector.detect_language_of(subject);
+
+            detected_language.map(|language| language.to_string())
+        }
+        _ => None,
+    }
 }
 
 #[magnus::init]
 fn init(ruby: &Ruby) -> Result<(), Error> {
     let module = ruby.define_module("Lingua")?;
-    module.define_singleton_method("detect", function!(detect, 1))?;
+    module.define_singleton_method("detect", function!(detect, -2))?;
     Ok(())
 }
--- test/test_lingua.rb
+++ test/test_lingua.rb
@@ -10,4 +10,41 @@ class TestLingua < Minitest::Test
   def test_it_detect_english
     assert_equal "English", Lingua.detect("this is definitely English")
   end
+
+  def test_it_detect_chinese
+    assert_equal "Chinese", Lingua.detect("你好，我来自中国")
+  end
+
+  def test_it_detect_hebrew
+    assert_equal "Hebrew", Lingua.detect("וזה בעברית")
+  end
+
+  def test_it_detect_polish_with_languages
+    assert_equal "Polish", Lingua.detect("państwowych", { languages: %i[english russian polish] })
+  end
+
+  def test_it_detect_nil
+    assert_nil Lingua.detect("כלב", { "languages" => %w[english russian polish] })
+  end
+
+  def test_it_support_minimum_relative_distance
+    assert_nil Lingua.detect(
+      "languages are awesome",
+      { "languages" => %w[english french german spanish], "minimum_relative_distance" => 0.9 }
+    )
+  end
+
+  def test_it_support_is_every_language_model_preloaded
+    assert "English", Lingua.detect(
+      "languages are awesome",
+      { "languages" => %w[english french german spanish], "is_every_language_model_preloaded" => true }
+    )
+  end
+
+  def test_it_support_is_low_accuracy_mode_enabled
+    assert "English", Lingua.detect(
+      "languages are awesome",
+      { "languages" => %w[english french german spanish], "is_low_accuracy_mode_enabled" => true }
+    )
+  end
 end
